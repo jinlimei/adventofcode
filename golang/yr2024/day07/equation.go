e67cbb907ab32b9fcc3eb79fb57357660434c420
Jinli mei <jinli@jinli.dev>
2025-01-13 00:45:23 -0700

    Start of day 7 part 1 kinda not-really
    This is mostly just a theorycraft and there's no way it'll work as-is

---
+++ golang/yr2024/day07/equation.go
@@ -0,0 +1,80 @@
+package day07
+
+type equationOp uint8
+
+const validOpsCount = 2
+
+const (
+	eqOpUnknown equationOp = iota
+	eqOpAdd
+	eqOpMultiply
+)
+
+func (op equationOp) String() string {
+	switch op {
+	case eqOpAdd:
+		return "+"
+	case eqOpMultiply:
+		return "*"
+	default:
+		panic("invalid equation op")
+	}
+}
+
+func (op equationOp) Exec(a, b int) int {
+	switch op {
+	case eqOpAdd:
+		return a + b
+	case eqOpMultiply:
+		return a * b
+	default:
+		panic("invalid equation op")
+	}
+}
+
+func validOps() [2]equationOp {
+	return [2]equationOp{
+		eqOpAdd,
+		eqOpMultiply,
+	}
+}
+
+type equation struct {
+	result int
+	nums   []int
+	ops    []equationOp
+	valid  bool
+}
+
+func newEquation() *equation {
+	return &equation{
+		nums: make([]int, 0),
+		ops:  make([]equationOp, 0),
+	}
+}
+
+func (eq *equation) solve() {
+	var (
+		nLen = len(eq.nums)
+		pos  = 0
+
+		n1 int
+		n2 int
+
+		vOps = validOps()
+	)
+
+	for ; pos < nLen-1; pos++ {
+		n1 = eq.nums[pos]
+		n2 = eq.nums[pos+1]
+
+		for k := 0; k < validOpsCount; k++ {
+			start := vOps[k].Exec(n1, n2)
+
+			for i := pos; i < nLen-1; i++ {
+				n1 = eq.nums[i]
+				n2 = eq.nums[i+1]
+			}
+		}
+	}
+}
--- golang/yr2024/day07/lib.go
+++ golang/yr2024/day07/lib.go
@@ -1,2 +1,54 @@
 package day07
 
+import (
+	"strconv"
+	"strings"
+)
+
+func parse(input string) []*equation {
+	input = strings.TrimSpace(input) + "\n"
+
+	var (
+		runes = []rune(input)
+		rLen  = len(runes)
+
+		pos = 0
+		buf = make([]rune, 0)
+		chr rune
+
+		num int
+
+		eqs = make([]*equation, 0)
+		eq  = newEquation()
+	)
+
+	for ; pos < rLen; pos++ {
+		chr = runes[pos]
+
+		switch chr {
+		case ':':
+			num, _ = strconv.Atoi(string(buf))
+			buf = make([]rune, 0)
+			eq.result = num
+
+			// we'll skip the space after ':'
+			if pos+1 < rLen && runes[pos+1] == ' ' {
+				pos++
+			}
+
+		case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+			buf = append(buf, chr)
+		case ' ', '\n':
+			num, _ = strconv.Atoi(string(buf))
+			buf = make([]rune, 0)
+			eq.nums = append(eq.nums, num)
+
+			if chr == '\n' {
+				eqs = append(eqs, eq)
+				eq = newEquation()
+			}
+		}
+	}
+
+	return eqs
+}
